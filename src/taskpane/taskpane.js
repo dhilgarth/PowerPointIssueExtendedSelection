69c08b9a71cbba6377f8b8c23afa608c1f3c263e
Daniel Hilgarth <d.hilgarth@sovarto.com>
2021-02-21 20:47:51 +0100

    Add code to demonstrate problem

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -11,26 +11,21 @@ import "../../assets/icon-80.png";
 /* global console, document, Office */
 
 Office.onReady(info => {
-  if (info.host === Office.HostType.PowerPoint) {
-    document.getElementById("sideload-msg").style.display = "none";
-    document.getElementById("app-body").style.display = "flex";
-    document.getElementById("run").onclick = run;
-  }
+    if (info.host === Office.HostType.PowerPoint) {
+        document.getElementById("sideload-msg").style.display = "none";
+        document.getElementById("app-body").style.display = "flex";
+        document.getElementById("run").onclick = run;
+    }
 });
 
 export async function run() {
-  /**
-   * Insert your PowerPoint code here
-   */
-  Office.context.document.setSelectedDataAsync(
-    "Hello World!",
-    {
-      coercionType: Office.CoercionType.Text
-    },
-    result => {
-      if (result.status === Office.AsyncResultStatus.Failed) {
-        console.error(result.error.message);
-      }
-    }
-  );
+    const info = document.getElementById('info');
+    info.innerHTML = "Navigating...";
+    Office.context.document.goToByIdAsync(Office.Index.Next, Office.GoToType.Index, {selectionMode: Office.SelectionMode.None}, result => {
+        if (result.status === Office.AsyncResultStatus.Failed) {
+            info.innerHTML = "Navigation failed";
+        } else {
+            info.innerHTML = "Navigation succeeded";
+        }
+    });
 }
